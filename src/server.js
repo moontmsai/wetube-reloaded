81f55b2d9277488139034cbdcf55b4dcf0f6787d
moontmsai <moontmsai@gmail.com>
2022-03-01 15:37:46 +0900

    3.7

--- src/server.js
+++ src/server.js
@@ -4,9 +4,30 @@ const PORT = 4000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.send("test message");
-});
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1> Not Allowed</h1>");
+  }
+  console.log("allowed, you may cotinue.");
+  next();
+};
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
