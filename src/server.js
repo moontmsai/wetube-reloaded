1831dab174252334bfa30009527f580311103479
moontmsai <moontmsai@gmail.com>
2022-03-01 16:30:03 +0900

    3.11 finish

--- src/server.js
+++ src/server.js
@@ -1,21 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1> Not Allowed</h1>");
-  }
-  console.log("allowed, you may cotinue.");
-  next();
-};
 const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
@@ -24,8 +14,6 @@ const handleProtected = (req, res) => {
 };
 
 app.use(logger);
-app.use(privateMiddleware);
-
 app.get("/", handleHome);
 app.get("/protected", handleProtected);
 
